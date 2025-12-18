8cb01177824f46aec2e875d1fadac5217571f574
Nurtimax <maksatbeknurtilek@gmail.com>
2025-12-18 13:53:07 +0600

    add geolocation

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -13,8 +13,16 @@ const config: CapacitorConfig = {
       src: "runners/background.js",
       event: "myCustomEvent",
       repeat: true,
-      interval: 15,
+      interval: 60, // Минимум 60 секунд для Android
       autoStart: true,
+    },
+    Geolocation: {
+      backgroundMessage: "Отслеживание местоположения в фоне",
+      backgroundTitle: "Геолокация активна",
+    },
+    LocalNotifications: {
+      smallIcon: "ic_stat_icon_config_sample",
+      iconColor: "#488AFF",
     },
   },
 };
